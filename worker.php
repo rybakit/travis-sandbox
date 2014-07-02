f4ccfc677558d0ab3525c4233e711f16d5817e7f
Eugene Leonovich <gen.work@gmail.com>
2014-07-02 20:31:45 +0200

    Update worker.php

--- worker.php
+++ worker.php
@@ -5,7 +5,7 @@ $worker->addServer('127.0.0.1');
 
 $workerId = uniqid(getmypid().'_', true);
 $worker->addFunction('pop', function(\GearmanJob $job) use ($workerId) {
-    echo "$workerId\n";
+    echo "$workerId: {$job->workload()}\n";
     return $workerId;
 });
 
