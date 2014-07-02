e377fad495d6bf19e50fce5e9cf40f1d3ae5d3eb
Eugene Leonovich <gen.work@gmail.com>
2014-07-02 10:48:54 +0200

    Update client.php

--- client.php
+++ client.php
@@ -1,9 +1,10 @@
 <?php
 
 $client = new GearmanClient();
+var_dump($client);exit;
+
 $result = $client->addServer();
 
-var_dump($result);
-exit;
+var_dump($result);exit;
 
 $client->doBackground('pop', 'Hello!');
