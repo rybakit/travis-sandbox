94cff1d1bd630d833551c626bc0984564efb20d7
Eugene Leonovich <gen.work@gmail.com>
2014-07-02 20:32:13 +0200

    Update client.php

--- client.php
+++ client.php
@@ -2,11 +2,12 @@
 
 $client = new GearmanClient();
 $result = $client->addServer('127.0.0.1');
-$client->setTimeout(30);
+//$client->setTimeout(30);
 
 var_dump($result);
 
-$h = $client->doBackground('pop', 'Hello!');
+$h = $client->doBackground('pop', 'Hello');
+$h = $client->doBackground('pop', 'World!');
 
 if ($client->returnCode() != GEARMAN_SUCCESS) {
   echo "bad return code\n";
