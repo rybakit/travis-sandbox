47be71512b934be12a7b76bdbc11659c22811dcb
Eugene Leonovich <rybakit@users.noreply.github.com>
2015-06-02 21:38:57 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,11 +1,4 @@
 <?php
 
-$c = new Tarantool('localhost', 3301);
-
-var_dump($c->connect());
-var_dump($c->connect());
-var_dump($c->ping());
-var_dump($c->close());
-var_dump($c->ping());
-var_dump($c->close());
-var_dump($c->close());
+$result = (new Tarantool())->authenticate('guest', 'incorrect_password');
+var_dump($result);
