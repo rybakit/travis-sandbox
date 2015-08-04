b20e3560fbd63242d0f13e2b4ea216cb30b6ce0a
Eugene Leonovich <rybakit@users.noreply.github.com>
2015-08-04 17:26:29 +0200

    Update box.lua

--- box.lua
+++ box.lua
@@ -6,3 +6,16 @@ box.cfg {
     wal_mode = 'none',
     snap_dir = '/tmp',
 }
+
+local function create_space(name)
+    if box.space[name] then
+        box.space[name]:drop()
+    end
+
+    return box.schema.space.create(name, {temporary = true})
+end
+
+space = create_space('space_foobar')
+space:create_index('primary', {type = 'hash', parts = {1, 'num'}})
+space:insert{1, 'foo'}
+space:insert{2, 'bar'}
