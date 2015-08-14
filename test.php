1892ecb8875cff80f74b3ea9d8a7a18a21f98140
Eugene Leonovich <rybakit@users.noreply.github.com>
2015-08-14 18:45:23 +0200

    Update test.php

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ namespace Foo;
 $unpacker = new \MessagePackUnpacker();
 $unpacker->setOption(\MessagePack::OPT_PHPONLY, false);
 
-$data = hex2bin('8200ce0000000001cf000000000000000080');
+$data = hex2bin('ce000000088200000100810102');
 
 $data = msgpack_unpack($data);
 //$data = $unpacker->unpack($data);
