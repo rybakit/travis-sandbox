c54980ed366255f67779329508160e1a71c7ff2e
Eugene Leonovich <rybakit@users.noreply.github.com>
2015-08-14 18:53:40 +0200

    Update test.php

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ namespace Foo;
 $unpacker = new \MessagePackUnpacker();
 $unpacker->setOption(\MessagePack::OPT_PHPONLY, false);
 
-$data = hex2bin('ce000000088200000100810102');
+$data = hex2bin('81a16102');
 
 $data = msgpack_unpack($data);
 //$data = $unpacker->unpack($data);
