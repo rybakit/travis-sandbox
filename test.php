785a171f7abe9d6954a4fd09d81f59c3fa2b71fb
Eugene Leonovich <rybakit@users.noreply.github.com>
2015-08-14 20:26:43 +0200

    Update test.php

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ $packer->setOption(\MessagePack::OPT_PHPONLY, false);
 
 $data = hex2bin('82c0a8737464436c617373a3666f6fa3626172');
 
-//$data = msgpack_unpack($data);
-$data = $packer->unpack($data);
+var_dump($packer->unpack($data));
 
-var_dump($data);
+ini_set('msgpack.php_only', 0);
+var_dump(msgpack_unpack($data));
