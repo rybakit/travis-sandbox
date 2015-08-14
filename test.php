f1538042ae6fc4918b1589c90e41e259a6008733
Eugene Leonovich <rybakit@users.noreply.github.com>
2015-08-14 18:37:59 +0200

    Update test.php

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ $unpacker->setOption(\MessagePack::OPT_PHPONLY, false);
 
 $data = hex2bin('8200ce0000000001cf000000000000000080');
 
-$data = msgpack_pack($data);
+$data = msgpack_unpack($data);
 //$data = $unpacker->unpack($data);
 
 var_dump($data);
