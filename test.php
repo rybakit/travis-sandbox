03d30f8ff1f7809a6bb5c5385b3f34c688e87b2f
Eugene Leonovich <rybakit@users.noreply.github.com>
2015-08-14 18:28:10 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,9 @@
 <?php
 
-class Test extends \PHPUnit_Framework_TestCase
-{
-    public function testFoo()
-    {
-        $this->assertSame('foo', 'foo');
-    }
-}
+namespace Foo;
+
+$unpacker = new \MessagePackUnpacker();
+$unpacker->setOption(\MessagePack::OPT_PHPONLY, false);
+
+$data = hex2bin('8200ce0000000001cf000000000000000080');
+var_dump($unpacker->unpack($data));
