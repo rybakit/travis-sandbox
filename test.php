361161cab486272dbccf8764f393f6d8a0e58a3c
Eugene Leonovich <rybakit@users.noreply.github.com>
2015-08-17 01:26:49 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,9 @@
 <?php
 
+$packer = new \MessagePack(true);
+var_dump($packer->unpack(hex2bin('8130dd0000000182a3666f6fa3626172c0a8737464436c617373')));
+
+/*
 $obj = (object) ['foo' => 'bar'];
 
 $packer = new \MessagePack(true);
@@ -12,3 +16,4 @@ $data = $packer->pack($obj);
 $unpacker->feed($data);
 var_dump($unpacker->execute());
 var_dump($unpacker->data());
+*/
