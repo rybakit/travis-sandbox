35febfa6fe1c97bde6db841ad0c42bb8575f6831
Eugene Leonovich <rybakit@users.noreply.github.com>
2015-10-07 22:55:29 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,28 @@
 <?php
 
+// https://github.com/msgpack/msgpack-php/issues/64
+
+class Obj
+{
+    public $a;
+    protected $b;
+    private $c;
+
+    function __construct($a, $b, $c) {
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+    }
+}
+
+$o = new Obj(1, 2, 3);
+
+$serialized = msgpack_serialize($o);
+$unserialized = msgpack_unserialize($serialized);
+var_dump($unserialized);
+exit;
+
+
 $packer = new \MessagePack(true);
 var_dump($packer->unpack(hex2bin('8130dd0000000182a3666f6fa3626172c0a8737464436c617373')));
 
